ecb8ac0b1a7af2f415bbfdcc12844a85c6c68678
garrethpb <gbroesche@gmail.com>
2018-09-26 15:27:13 -0500

    urls.py : create path for logout

--- apps/musicians/url.py
+++ apps/musicians/url.py
@@ -5,4 +5,5 @@ urlpatterns = [
 	path('welcome/', welcome.main, name="welcome"),
 	path('register/', register.main, name="register"),
 	path('login/', login.main, name="login"),
+	path('logout/', logout.main, name="logout"),
 ]
