c14a7e3f14fc5b741a7cbafa4575491fec94d477
garrethpb <gbroesche@gmail.com>
2018-09-26 15:30:52 -0500

    views.py : create welcome

--- apps/musicians/views.py
+++ apps/musicians/views.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.shortcuts import render
 
-# Create your views here.
+# localhost/musicians/welcome/
+def welcome(req):
+	return render(req, 'html/welcome.html')
