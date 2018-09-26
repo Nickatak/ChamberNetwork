f5c123f8da95149267c19d50a098890ce8ff32b0
garrethpb <gbroesche@gmail.com>
2018-09-26 15:16:03 -0500

    settings.py install app performances

--- chambernet/settings.py
+++ chambernet/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apps.musicians',
+    'apps.performances',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
