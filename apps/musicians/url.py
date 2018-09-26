38c0efc585f3d6aa07f6c8bfa65e77f74820b3c3
garrethpb <gbroesche@gmail.com>
2018-09-26 15:26:37 -0500

    urls.py : create path for login

--- apps/musicians/url.py
+++ apps/musicians/url.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	path('welcome/', welcome.main, name="welcome"),
 	path('register/', register.main, name="register"),
+	path('login/', login.main, name="login"),
 ]
