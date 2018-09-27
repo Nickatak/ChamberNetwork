8f94f04d2e14fe5771eb291f7fb50f05aeb2fbe5
garrethpb <gbroesche@gmail.com>
2018-09-27 16:51:19 -0500

    urls.py : create path for faq

--- apps/musicians/urls.py
+++ apps/musicians/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
 	path('logout/', views.logout, name="logout"),
 	path('musician/', views.musician, name="musician"),
 	path('dashboard/', views.dashboard, name="dashboard"),
+	path('faq/', views.faq, name="faq"),
 ]
