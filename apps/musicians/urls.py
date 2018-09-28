165fb4de77704542a56494a642653233fe988026
garrethpb <gbroesche@gmail.com>
2018-09-28 16:12:17 -0500

    urls.py : change register path

--- apps/musicians/urls.py
+++ apps/musicians/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('welcome/', views.welcome, name="welcome"),
-	path('register/', views.register, name="register"),
+	path('register_member/', views.register_member, name="register_member"),
 	path('login/', views.login, name="login"),
 	path('logout/', views.logout, name="logout"),
 	path('musician/', views.musician, name="musician"),
