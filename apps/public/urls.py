f0a81887bd46072911edb93262006608deaf7ac5
garrethpb <gbroesche@gmail.com>
2018-09-29 15:17:26 -0500

    2 urls.py files : shift some paths to public app

---
+++ apps/public/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('welcome/', views.welcome, name="welcome"),
+	path('register_member/', views.register_member, name="register_member"),
+	path('register_coach/', views.register_coach, name="register_coach"),
+	path('about/', views.about, name="about"),
+	path('contact/', views.contact, name="contact"),
+]
