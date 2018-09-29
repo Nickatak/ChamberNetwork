abbee2aa53416feb52f051a13b965ccd54f8689d
garrethpb <gbroesche@gmail.com>
2018-09-29 15:19:24 -0500

    2 views.py files : shift some views to public

--- apps/public/views.py
+++ apps/public/views.py
@@ -1,3 +1,23 @@
+from __future__ import unicode_literals
+
 from django.shortcuts import render
 
-# Create your views here.
+# localhost/public/welcome/
+def welcome(req):
+	return render(req, 'html/welcome.html')
+
+# localhost/public/register_member/
+def register_member(req):
+	return render(req, 'html/register_member.html')
+
+# localhost/public/register_coach/
+def register_coach(req):
+	return render(req, 'html/register_coach.html')
+
+# localhost/public/about/
+def about(req):
+	return render(req, 'html/about.html')
+
+# localhost/public/contact/
+def contact(req):
+	return render(req, 'html/contact.html')
