666e3c6aae89d06aae7e912934719e028a9c7a62
garrethpb <gbroesche@gmail.com>
2018-10-18 15:48:42 -0500

    views.py : create view for register_supporter

--- apps/public/views.py
+++ apps/public/views.py
@@ -2,18 +2,24 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# localhost/public/welcome/
+# houstonchambermusic.org/public/welcome/
 def welcome(req):
 	return render(req, 'html/welcome.html')
 
-# localhost/public/register_member/
+# houstonchambermusic.org/public/register_member/
 def register_member(req):
 	return render(req, 'html/register_member.html')
 
-# localhost/public/register_coach/
+# houstonchambermusic.org/public/register_coach/
 def register_coach(req):
 	return render(req, 'html/register_coach.html')
 
-# localhost/public/about/
+# houstonchambermusic.org/public/register_supporter
+def register_supporter(req):
+	return render(req, 'html/register_supporter.html')
+
+# houstonchambermusic.org/public/about/
 def about(req):
 	return render(req, 'html/about.html')
+
+
