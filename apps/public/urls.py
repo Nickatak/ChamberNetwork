5dc97cceae51037faf8517879df99392e4464189
garrethpb <gbroesche@gmail.com>
2018-10-18 15:46:53 -0500

    urls.py : add path for register_supporter

--- apps/public/urls.py
+++ apps/public/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 	path('welcome/', views.welcome, name="welcome"),
 	path('register_member/', views.register_member, name="register_member"),
 	path('register_coach/', views.register_coach, name="register_coach"),
+	path('register_supporter', views.register_supporter, name="register_supporter"),
 	path('about/', views.about, name="about"),
 ]
