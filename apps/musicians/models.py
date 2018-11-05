f9c07f9f6b8e6b174f4685297b7b485592b2c3cc
garrethpb <gbroesche@gmail.com>
2018-11-05 15:10:18 -0600

    models.py

--- apps/musicians/models.py
+++ apps/musicians/models.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
-from django.db import models
-from django.contrib.auth.hashers import make_password, check_password
 
-import re
 
-NAME_REGEX = re.compile(r"^[-a-zA-Z']+$")
-EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$")
--- apps/public/models.py
+++ apps/public/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+
+import re
+
+NAME_REGEX = re.compile(r"^[-a-zA-Z']+$")
+EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$")
