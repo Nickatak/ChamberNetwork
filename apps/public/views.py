0d284cb0503cedefb27262f01548c637525b1318
garrethpb <gbroesche@gmail.com>
2018-11-09 16:33:02 -0600

    views

--- apps/public/views.py
+++ apps/public/views.py
@@ -4,31 +4,65 @@ from django.shortcuts import render
 
 # houstonchambermusic.org/public/welcome/
 def welcome(req):
-	return render(req, 'html/welcome.html')
-	render(req, 'template.name', page_name='Welcome')
+
+	context = {
+		'page_name' : 'Welcome',
+	}
+
+	return render(req, 'html/welcome.html', context)
 
 # houstonchambermusic.org/public/register_member/
 def register_member(req):
-	return render(req, 'html/register_member.html')
+
+	context = {
+		'page_name' : 'Register Member',
+	}
+
+	return render(req, 'html/register_member.html', context)
 
 # houstonchambermusic.org/public/register_coach/
 def register_coach(req):
-	return render(req, 'html/register_coach.html')
+
+	context = {
+		'page_name' : 'Register Coach',
+	}
+
+	return render(req, 'html/register_coach.html', context)
 
 # houstonchambermusic.org/public/register_patron/
 def register_patron(req):
-	return render(req, 'html/register_patron.html')
+
+	context = {
+		'page_name' : 'Register Patron',
+	}
+
+	return render(req, 'html/register_patron.html', context)
 
 # houstonchambermusic.org/public/about/
 def about(req):
-	return render(req, 'html/about.html')
+
+	context = {
+		'page_name' : 'About Us',
+	}
+
+	return render(req, 'html/about.html', context)
 
 # houstonchambermusic.org/public/success/
 def success(req):
-	return render(req, 'html/success.html')
+
+	context = {
+		'page_name' : 'Success',
+	}
+
+	return render(req, 'html/success.html', context)
 
 # houstonchambermusic.org/public/login/
 def login(req):
-	return render(req, 'html/login.html')
+
+	context = {
+		'page_name' : 'Login',
+	}
+
+	return render(req, 'html/login.html', context)
 
 
