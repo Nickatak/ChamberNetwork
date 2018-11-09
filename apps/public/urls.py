74aa8b6beca66c6f2924b5340553ebacd434cc7f
garrethpb <gbroesche@gmail.com>
2018-11-09 11:46:45 -0600

    base.html

--- apps/public/urls.py
+++ apps/public/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView
+# from django.contrib.auth.views import LoginView
 
 urlpatterns = [
 	path('welcome/', views.welcome, name="welcome"),
