08049e673cbebf9c4e9a3dc1ba7a24c93520302e
garrethpb <gbroesche@gmail.com>
2018-11-12 17:03:01 -0600

    working on login/logout

--- apps/musicians/urls.py
+++ apps/musicians/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('logout/', views.logout, name="logout"),
 	path('musician/', views.musician, name="musician"),
 	path('dashboard/', views.dashboard, name="dashboard"),
 ]
