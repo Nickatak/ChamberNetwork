3d9536bcca5d69dec8f4dd6f362f370522f758e4
Nickatak <nickle87@gmail.com>
2018-11-15 15:12:01 -0800

    Revert.

--- apps/public/urls.py
+++ apps/public/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
 	path('register_coach/', views.register_coach, name="register_coach"),
 	path('register_patron/', views.register_patron, name="register_patron"),
 	path('success/', views.success, name="success"),
-	# path('login/', LoginView.as_view(template_name='public/login.html'), name="login"),
 	path('login/', views.login, name="login"),
 	path('about/', views.about, name="about"),
 ]
--- chambernet/settings.py
+++ chambernet/settings.py
@@ -122,5 +122,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/musicians/dashboard/'
-LOGOUT_REDIRECT_URL = '/accounts/login/'
