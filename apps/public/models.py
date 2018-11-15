2df1b73a58dd91f821f692035fd32fcc770105d9
garrethpb <gbroesche@gmail.com>
2018-11-15 15:46:10 -0600

    models.py

--- apps/public/models.py
+++ apps/public/models.py
@@ -4,11 +4,6 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 
-import re
-
-NAME_REGEX = re.compile(r"^[-a-zA-Z']+$")
-EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$")
-
 # Patron Manager Model
 class PatronManager(models.Manager):
 	pass
