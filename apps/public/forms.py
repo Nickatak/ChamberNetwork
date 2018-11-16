89fcb5f75983e1e329829059007b8e11b1698b0c
garrethpb <gbroesche@gmail.com>
2018-11-16 16:28:06 -0600

    forms.py

--- apps/public/forms.py
+++ apps/public/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
-class MusicianLoginForm(forms.form):
+class MusicianLoginForm(forms.Form):
 	username = forms.CharField(max_length=100)
 	password = forms.CharField(max_length=100)
