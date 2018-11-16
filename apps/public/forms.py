29518f093cf9ed23f528c4d6cf6beab3e233c1b3
garrethpb <gbroesche@gmail.com>
2018-11-16 16:54:47 -0600

    forms.py

--- apps/public/forms.py
+++ apps/public/forms.py
@@ -5,18 +5,18 @@ class MusicianLoginForm(forms.Form):
 	username = forms.CharField(max_length=100)
 	password = forms.CharField(widget=forms.PasswordInput())
 
-def clean(self):
-	data_dict = super().clean()
+	def clean(self):
+		data_dict = super().clean()
 
-	if len(data_dict['username']) < 1:
+		if len(data_dict['username']) < 1:
 			errors['username'] = 'You must enter a username.'
 
-	if len(data_dict['password']) < 1:
-		erros['password'] = "You must enter a password."
+		if len(data_dict['password']) < 1:
+			erros['password'] = "You must enter a password."
 
-def login_musician(self):
-	if username belongs to X and if password belongs to X:
-		log in user
-	else:
-		do something else
+		if username belongs to X and if password belongs to X:
+			log in user
+			
+		else:
+			do something else
 
