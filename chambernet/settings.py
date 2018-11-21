13831ef16af01164806538ea00351ed80f700200
Nickatak <nickle87@gmail.com>
2018-11-21 00:54:14 -0800

    settings.py: Temporarily disable other apps.

--- chambernet/settings.py
+++ chambernet/settings.py
@@ -33,8 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'apps.public',
     'apps.users',
-    'apps.performances',
-    'apps.instruments',
+    #'apps.performances',
+    #'apps.instruments',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
