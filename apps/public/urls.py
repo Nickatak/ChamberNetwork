58f5b4e19d0d9d04ff9000cf11e480286c1e8b6e
Nickatak <nickle87@gmail.com>
2018-11-21 00:38:46 -0800

    urls.py: Change register to new.

--- apps/public/urls.py
+++ apps/public/urls.py
@@ -7,9 +7,9 @@ app_name = 'public'
 urlpatterns = [
     #Index page has no post-fix.
 	path('', views.welcome, name="welcome"),
-	path('register_member/', views.register_member, name="register_member"),
-	path('register_coach/', views.register_coach, name="register_coach"),
-	path('register_patron/', views.register_patron, name="register_patron"),
+	path('new_member/', views.register_member, name="new_member"),
+	path('new_coach/', views.register_coach, name="new_coach"),
+	path('new_patron/', views.register_patron, name="new_patron"),
 	path('success/', views.success, name="success"),
 	path('login/', views.login, name="login"),
 	path('logout/', views.logout, name="logout"),
