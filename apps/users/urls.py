9ecdda86251e0944695e60e15f5df5748394b088
Nickatak <nickle87@gmail.com>
2018-11-21 00:46:25 -0800

    urls.py: Add basic handler routes.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,5 +3,8 @@ from .  import views
 
 # All functional views go here.
 urlpatterns = [
-    path('process_login/', views.process_login),
+    path('register_member/', views.register_member),
+    path('register_patron/', views.register_patron),
+    path('register_coach/', views.register_coach),
+    path('login_handler/', views.process_login),
 ]
