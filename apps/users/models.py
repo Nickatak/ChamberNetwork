47a626e9e3ef854496063537c7c547c9b2db8866
Nickatak <nickle87@gmail.com>
2018-11-20 23:29:40 -0800

    Add users app.

---
+++ apps/users/models.py
@@ -0,0 +1,67 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.models import User
+
+# Patron Manager Model
+# class PatronManager(models.Manager):
+# 	pass
+
+# Musician Manager Model
+class MusicianManager(models.Manager):
+	pass
+
+# Patron Model. This is just for an emailing list right?  All we need to do is store them then, not a big deal.
+class Patron(models.Model):
+ 	first_name = models.CharField(max_length=25)
+ 	last_name = models.CharField(max_length=25)
+ 	email = models.CharField(max_length=25)
+ 	phone_number = models.CharField(max_length=10)
+ 	street_address = models.CharField(max_length=25)
+ 	unit_number = models.CharField(max_length=25)
+ 	city = models.CharField(max_length=25)
+ 	state = models.CharField(max_length=2)
+ 	zip_code = models.CharField(max_length=5)
+
+	# referred_by : does this belong here or in musician table?
+	# referred_by = models.ForeignKey("Referred by", on_delete=models.CASCADE, related_name="musician")
+
+	# created_at = models.DateTimeField(auto_now_add=True)
+	# updated_at = models.DateTimeField(auto_now=True)
+
+	# objects = PatronManager()
+
+# Musician Manager Model
+class MusicianManager(models.Manager):
+	pass
+
+# Musician Model
+class Musician(models.Model):
+    #Will the email be used as auth?
+ 	email = models.CharField(max_length=25)
+ 	first_name = models.CharField(max_length=25)
+ 	last_name = models.CharField(max_length=25)
+ 	phone_number = models.CharField(max_length=10)
+ 	street_address = models.CharField(max_length=25)
+ 	unit_number = models.CharField(max_length=25)
+ 	city = models.CharField(max_length=25)
+ 	state = models.CharField(max_length=2)
+ 	zip_code = models.CharField(max_length=5)
+
+	bio = models.TextField()
+
+	# Approval : not sure about how to set this up. The idea is that it defaults to False but is changed to True by admin once musician is approved
+	is_approved = models.BooleanField(default=False)
+    
+	# Primary and secondary instruments: FK set up correctly?
+	# primary_instrument = models.ForeignKey("Primary instrument", on_delete=models.CASCADE, related_name="musicians")
+	# secondary_instrument = models.ForeignKey("Secondary instrument", on_delete=models.CASCADE, related_name="musicians")
+
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	objects = MusicianManager()
+
+
+	objects = MusicianManager()
---
+++ apps/users/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import path
+from .  import views
+
+# All functional views go here.
+urlpatterns = [
+    path('process_login/', views.process_login),
+]
