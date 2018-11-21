6ba8d564e73e1dc645a6fae75cbad04378ef529e
Nickatak <nickle87@gmail.com>
2018-11-21 00:54:22 -0800

    Temporary change.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,9 +3,15 @@ from .  import views
 
 # All functional views go here.
 urlpatterns = [
+    path('login_handler/', views.login_handler),
+
+]
+
+'''
+    URLS to be implemented:
     path('register_member/', views.register_member),
     path('register_patron/', views.register_patron),
     path('register_coach/', views.register_coach),
-    path('login_handler/', views.login_handler),
+
     path('logout_handler/', views.logout_handler),
-]
+'''
