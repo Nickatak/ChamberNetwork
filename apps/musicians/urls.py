3a9774cf518a3779f6eccee796e2f6968253eb7b
Nickatak <nickle87@gmail.com>
2018-11-20 23:29:58 -0800

    Remove musicians app.

--- apps/musicians/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('musician/', views.musician, name="musician"),
-	path('dashboard/', views.dashboard, name="dashboard"),
-]
