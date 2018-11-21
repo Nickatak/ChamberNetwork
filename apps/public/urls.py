88cccb9de3fe4d90ce5594ef693b210d7449da75
Nickatak <nickle87@gmail.com>
2018-11-21 00:51:11 -0800

    urls.py: Change bound function names.

--- apps/public/urls.py
+++ apps/public/urls.py
@@ -7,11 +7,10 @@ app_name = 'public'
 urlpatterns = [
     #Index page has no post-fix.
 	path('', views.welcome, name="welcome"),
-	path('new_member/', views.register_member, name="new_member"),
-	path('new_coach/', views.register_coach, name="new_coach"),
-	path('new_patron/', views.register_patron, name="new_patron"),
+	path('new_member/', views.new_member_display, name="new_member"),
+	path('new_coach/', views.new_member_display, name="new_coach"),
+	path('new_patron/', views.new_member_display, name="new_patron"),
 	path('success/', views.success, name="success"),
-	path('login/', views.login, name="login"),
-	path('logout/', views.logout, name="logout"),
+	path('login/', views.login_display, name="login"),
 	path('about/', views.about, name="about"),
 ]
