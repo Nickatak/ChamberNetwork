3ddcd1b51851b450481e37f1c7b05fde9636fa68
garrethpb <gbroesche@gmail.com>
2018-11-25 16:23:17 -0600

    models.py : work on Member model and MemberManager

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,8 +2,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
+import re
+
+NAME_REGEX = re.compile(r"^[-a-zA-Z']+$")
+EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$")
 
 
 # Patron Model. This is just for an emailing list right?  All we need to do is store them then, not a big deal.
@@ -43,7 +47,40 @@ class MemberManager(models.Manager):
         errors = {}
 
 
-        
+        # Ensure that first name 1) has content 2) is at least 2 characters long and 3) contains no invalid characters:
+        if len(postData['first_name']) < 1:
+            errors['first_name'] = 'You must enter a first name.'
+        elif len(postData['first_name']) < 2:
+            errors['first_name'] = 'First name must contain at least two characters.'
+        elif not NAME_REGEX.match(postData['first_name']):
+            errors['first_name'] = 'First name contains an invalid character.'
+
+        # Ensure that last name 1) has content 2) is at least 2 characters long and 3) contains no invalid characters:
+        if len(postData['last_name']) < 1:
+            errors['last_name'] = 'You must enter a last name.' 
+        elif len(postData['last_name']) < 2:
+            errors['last_name'] = 'Last name must contain at least two characters.'
+        elif not NAME_REGEX.match(postData['last_name']):
+            errors['last_name'] = 'Last name contains an invalid character.'
+
+        # Ensure that email field is completed and that email adheres to standard conventions:
+        if len(postData['email']) < 1:
+            errors['email'] = 'You must enter an email address.'
+        elif not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = 'Please enter a valid email address.'
+        # Call email_exists to ensure they haven't already signed up
+        elif self.email_exists(postData['email']) == True:
+            errors['email'] = 'Email already found in database. Either enter a different email or go to sign in page.'
+
+        return errors
+
+    def add_member(self, data):
+        first_name = data['first_name']
+        last_name = data['last_name']
+        email = data['email']
+
+        return self.create(first_name=first_name, last_name=last_name, email=email)
+
 
 # Member Model
 class Member(models.Model):
