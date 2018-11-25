f9d79c06a0a7fa39866d3c74922c839815508d41
garrethpb <gbroesche@gmail.com>
2018-11-25 17:01:23 -0600

    register_member.html : add csrf token

--- apps/users/views.py
+++ apps/users/views.py
@@ -8,19 +8,19 @@ def login_handler(req):
     pass
 
 # houstonchambermusic.org/new_member/
-# def register_member(req):
-# 	if request.method == "POST":
-# 		errors = Member.objects.member_validation(request.POST)
+def register_member(req):
+	if request.method == "POST":
+		errors = Member.objects.member_validation(request.POST)
 
-# 		if not errors:
-# 			Member.objects.add_member(request.POST)
-# 			# not sure if this is the correct route:
-# 			return redirect('/users/login')
-# 		else:
-# 			context = {
-# 				'errors' : errors
-# 			}
-# 			return render(req, 'html/register_member.html', context=context)
+		if not errors:
+			Member.objects.add_member(request.POST)
+			# not sure if this is the correct route:
+			return redirect('/users/login')
+		else:
+			context = {
+				'errors' : errors
+			}
+			return render(req, 'html/register_member.html', context=context)
 
-# 	elif request.method == "GET":
-# 		return render(req, 'html/register_member.html')
+	elif request.method == "GET":
+		return render(req, 'html/register_member.html')
