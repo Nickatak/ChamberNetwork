bae3ff496df46cae0d7f575d9687b906ba43ef93
Nickatak <nickle87@gmail.com>
2018-12-16 06:03:43 -0800

    admin.py: Add creation date to list display.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -4,7 +4,7 @@ from .models import Member
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'is_reviewed', 'is_approved',)
+    list_display = ('first_name', 'last_name', 'is_reviewed', 'is_approved', 'created_at', 'updated_at')
 
     actions = ['approve_all', 'disapprove_all']
 
