5923266ad8bc346cba7c0e41df6b5b051cdc6643
Nickatak <nickle87@gmail.com>
2018-12-16 06:59:29 -0800

    urls.py: Add coach registration handler route.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,6 +6,7 @@ app_name = 'users'
 # All functional views go here.
 urlpatterns = [
     path('login_handler/', views.login_handler, name="login_handler"),
+    path('register_coach/', views.register_coach, name="coach_registration_handler"),
     path('register_member/', views.register_member, name="member_registration_handler"),
     path('register_patron/', views.register_patron, name="patron_registration_handler"),
     path('dashboard/', views.dashboard, name="dashboard"),
