8a0c0b7721215bb8ca36136187860580366e2e0d
Nickatak <nickle87@gmail.com>
2018-12-16 06:51:45 -0800

    urls.py: Add route names.

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,9 +4,9 @@ from .  import views
 # All functional views go here.
 urlpatterns = [
     path('login_handler/', views.login_handler, name="login_handler"),
-    path('register_member/', views.register_member),
-    path('register_patron/', views.register_patron),
-    path('dashboard/', views.dashboard),
+    path('register_member/', views.register_member, name="member_registration_handler"),
+    path('register_patron/', views.register_patron, name="patron_registration_handler"),
+    path('dashboard/', views.dashboard, name="dashboard"),
 
 ]
 
