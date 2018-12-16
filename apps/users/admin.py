8ba3cc7241311d11c8482911179cf0b29d91f1bc
Nickatak <nickle87@gmail.com>
2018-12-16 05:52:44 -0800

    admin.py: Add approve-all functtion.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -5,3 +5,11 @@ from .models import Member
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'is_reviewed', 'is_approved',)
+
+    actions = ['approve_all']
+
+    def approve_all(self, request, selected_members):
+        selected_members.update(is_approved=True, is_reviewed=True)
+    approve_all.short_description= 'Approve and review all checked members.'
+
+
