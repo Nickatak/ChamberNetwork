b0145a9c0be42232b90c1d1eba43c8bbd4a0e9f2
Nickatak <nickle87@gmail.com>
2018-12-16 05:56:00 -0800

    admin.py: Add denial function.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -6,10 +6,12 @@ from .models import Member
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'is_reviewed', 'is_approved',)
 
-    actions = ['approve_all']
+    actions = ['approve_all', 'disapprove_all']
 
     def approve_all(self, request, selected_members):
         selected_members.update(is_approved=True, is_reviewed=True)
-    approve_all.short_description= 'Approve and review all checked members.'
+    approve_all.short_description = 'Approve and review all checked members.'
 
-
+    def disapprove_all(self, request, selected_members):
+        selected_members.update(is_approved=False, is_reviewed=True)
+    disapprove_all.short_description = 'Deny and review all checked members.'
