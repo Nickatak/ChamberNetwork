fb2ad36abc5c3ccf38ec3fa4a44edfede25d7a04
Nickatak <nickle87@gmail.com>
2019-01-02 15:55:57 -0800

    views.py: Pass instrument to template.

--- apps/instruments/views.py
+++ apps/instruments/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+from .models import Instrument
 # Create your views here.
 
 def dummy(req):
     pass
 
 def individual_instrument(req, instrument_id):
-    print(instrument_id)
+    # Add a safeguard for this later.
+    context = {
+        'instrument' : Instrument.objects.get(id=int(instrument_id)),
+    }
     return render(req, 'html/individual_instrument.html')
