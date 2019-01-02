0bae8ec847d4163b5b179f8a8156ce26aa821d54
garrethpb <gbroesche@gmail.com>
2019-01-02 17:01:45 -0600

    urls.py : path for individual instrument view

--- apps/instruments/urls.py
+++ apps/instruments/urls.py
@@ -6,5 +6,5 @@ app_name = 'instruments'
 
 url_patterns = [
     path('', views.dummy),
-    path('instruments/(?P\d+)', views.individual_instrument, name="individual_instrument"),
+    path('/(?P\d+)', views.individual_instrument, name="individual_instrument"),
 ]
