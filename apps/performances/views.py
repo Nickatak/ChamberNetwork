869a47fa2cb1d0ad5192f53dc53017f927d4e140
garrethpb <gbroesche@gmail.com>
2019-01-03 14:33:56 -0600

    performances app : create folders and files for templates and static

--- apps/performances/views.py
+++ apps/performances/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+# from .models import Performance
 
 # Create your views here.
+
+def add(req):
+	return render(req, 'html/add.html')
+
+def upcoming(req):
+	return render(req, 'html/upcoming.html')
