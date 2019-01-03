ea4e0080394c74753eca561861aeb5e7a897600c
garrethpb <gbroesche@gmail.com>
2019-01-03 15:12:31 -0600

    trash not used yet folder for performances

--- not_used_yet/performances/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('add/', views.add, name="add"),
-	path('performance/', views.performance, name="performance"),
-	path('upcoming/', views.upcoming, name="upcoming"),
-]
