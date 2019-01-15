7e0d25385502864e20954e660a1e03a22d1f17ad
Nickatak <nickle87@gmail.com>
2019-01-15 14:59:46 -0800

    Add test-users.

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,9 +1,11 @@
 from functools import update_wrapper
+import random
 
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path
 from .models import Member, Patron
+from ..instruments.models import Instrument
 # Register your models here.
 
 @admin.register(Member)
@@ -40,7 +42,69 @@ class MemberAdmin(admin.ModelAdmin):
         #Since django does left-right (in-order) scanning of urls, it's important to put our custom ones first.
         return custom_urls + default_urls
 
+    # Dirty test-users method, will replace later.  Clean it up and put it in a test suite.
     def import_test_users(self, req):
+    
+        with open('test_data.txt', 'r') as fo:
+            raw_data = fo.read().split('\n')
+
+        split_data = []
+        data_set = []
+        i = 0
+
+        while i < len(raw_data):
+            if not raw_data[i]:
+                if data_set:
+                    split_data.append(data_set)
+                data_set = []
+            else:
+                data_set.append(raw_data[i])
+                
+            i += 1
+
+            
+            
+        test_users = []
+        for user_data in split_data:
+            test_user = {}
+            
+            test_user['first_name'] = user_data[0].split(' ')[0]
+            test_user['last_name'] = user_data[0].split(' ')[1]
+            test_user['email'] = user_data[5]
+            test_user['street_address'] = user_data[2]
+            test_user['city'] = user_data[3]
+            test_user['state'] = 'TX'
+            test_user['zip_code'] = user_data[4]
+
+            test_user['phone_number'] = user_data[1].replace('-', '')
+
+            test_user['bio'] = 'Some bio'
+            test_user['rating'] = random.choice(['A', 'B', 'C', 'D', 'E'])
+
+            rand_coach = [False]
+            if test_user['rating'] == 'A':
+                rand_coach.append(True)
+            test_user['is_coach'] = random.choice(rand_coach)
+
+            primary_instruments_available = Instrument.objects.filter(primary_users__isnull=True)
+            if not primary_instruments_available:
+                primary_instruments_available = Instrument.objects.all()
+            test_user['primary_instrument'] = random.choice(primary_instruments_available)
+            
+            secondary_instruments_available = Instrument.objects.filter(secondary_users__isnull=True)
+            if not secondary_instruments_available:
+                secondary_instruments_available = Instrument.objects.all()
+            test_user['secondary_instrument'] = random.choice(secondary_instruments_available)
+            
+            
+            
+            
+            
+            test_users.append(test_user)
+
+        for test_user in test_users:
+            Member.objects.create(**test_user)
+
         return redirect('..')
 
 
