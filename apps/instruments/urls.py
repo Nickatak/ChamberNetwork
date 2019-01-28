8d3e3e6dc36fe16c178924aabc960b2de7e7f7c6
Nickatak <nickle87@gmail.com>
2019-01-28 11:35:11 -0800

    urls.py: Clean file

--- apps/instruments/urls.py
+++ apps/instruments/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, re_path
+
 from . import views
 
 
 app_name = 'instruments'
-
 urlpatterns = [
     re_path('(?P<instrument_id>\d+)', views.individual_display, name="individual_display"),
 ]
