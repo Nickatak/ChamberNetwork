7caad154b86be5a5daf0a6ae544661f2f35c24b2
Nickatak <nickle87@gmail.com>
2019-01-28 11:33:34 -0800

    admin.py: Clean file

--- apps/instruments/admin.py
+++ apps/instruments/admin.py
@@ -3,9 +3,9 @@ from functools import update_wrapper
 from django.contrib import admin
 from django.shortcuts import redirect
 from django.urls import path
+
 from .models import Instrument
 
-# Register your models here.
 
 @admin.register(Instrument)
 class InstrumentAdmin(admin.ModelAdmin):
@@ -16,20 +16,21 @@ class InstrumentAdmin(admin.ModelAdmin):
 
     # Override parent default url handlers to add my own.
     def get_urls(self):
+
         # Security wrapper for django, otherwise the route will be unprotected
         def wrap(view):
             def wrapper(*args, **kwargs):
                 return self.admin_site.admin_view(view)(*args, **kwargs)
-            
+
             wrapper.model_admin = self
             return update_wrapper(wrapper, view)
-    
+
         default_urls = super().get_urls()
-        
+
         custom_urls = [
             path('import-instruments/', wrap(self.import_instruments), name="import_instruments"),
         ]
-        
+
         #Since django does left-right (in-order) scanning of urls, it's important to put our custom ones first.
         return custom_urls + default_urls
 
@@ -40,9 +41,9 @@ class InstrumentAdmin(admin.ModelAdmin):
         # Easier to use a disjoint set algo here than query for every instrument.
         from .instruments_list import instruments
         container = {}
-        
+
         db_instruments = Instrument.objects.all()
-        
+
         # Store the db's instrument-names inside a hash table.
         for db_instrument in db_instruments:
             container[db_instrument.name] = 0
@@ -54,12 +55,11 @@ class InstrumentAdmin(admin.ModelAdmin):
                 container[instrument_name] + 1
             except KeyError:
                 Instrument.objects.create(name=instrument_name)
-        
+
         # Rotate through the DB instruments in the hash table:
         for instrument_name in container:
             # Prune and remove instruments that are in the DB that don't match the maintained list.
             if instrument_name not in instruments:
                 Instrument.objects.get(name=instrument_name).delete()
-        
+
         return redirect('..')
-                
