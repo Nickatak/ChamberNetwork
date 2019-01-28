0c1026a206d47f486685841253f1b6e0efef5e3e
Nickatak <nickle87@gmail.com>
2019-01-28 11:23:23 -0800

    admin.py: Revamp test users import

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -45,46 +45,9 @@ class MemberAdmin(admin.ModelAdmin):
     # Dirty test-users method, will replace later.  Clean it up and put it in a test suite.
     def import_test_users(self, req):
     
-        with open('test_data.txt', 'r') as fo:
-            raw_data = fo.read().split('\n')
-
-        split_data = []
-        data_set = []
-        i = 0
-
-        while i < len(raw_data):
-            if not raw_data[i]:
-                if data_set:
-                    split_data.append(data_set)
-                data_set = []
-            else:
-                data_set.append(raw_data[i])
-                
-            i += 1
-
-            
-            
-        test_users = []
-        for user_data in split_data:
-            test_user = {}
-            
-            test_user['first_name'] = user_data[0].split(' ')[0]
-            test_user['last_name'] = user_data[0].split(' ')[1]
-            test_user['email'] = user_data[5]
-            test_user['street_address'] = user_data[2]
-            test_user['city'] = user_data[3]
-            test_user['state'] = 'TX'
-            test_user['zip_code'] = user_data[4]
-
-            test_user['phone_number'] = user_data[1].replace('-', '')
-
-            test_user['bio'] = 'Some bio'
-            test_user['rating'] = random.choice(['A', 'B', 'C', 'D', 'E'])
-
-            rand_coach = [False]
-            if test_user['rating'] == 'A':
-                rand_coach.append(True)
-            test_user['is_coach'] = random.choice(rand_coach)
+        from .test_users import test_users
+        
+        for test_user in test_users:
 
             primary_instruments_available = Instrument.objects.filter(primary_users__isnull=True)
             if not primary_instruments_available:
@@ -96,11 +59,6 @@ class MemberAdmin(admin.ModelAdmin):
                 secondary_instruments_available = Instrument.objects.all()
             test_user['secondary_instrument'] = random.choice(secondary_instruments_available)
             
-            
-            
-            
-            
-            test_users.append(test_user)
 
         for test_user in test_users:
             Member.objects.create(**test_user)
