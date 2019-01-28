f3565fc28fdff32eca255bdacf6b9d200f1a2981
Nickatak <nickle87@gmail.com>
2019-01-28 09:42:52 -0800

    models.py: Add basic email model

--- apps/emails/models.py
+++ apps/emails/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class EmailManager(models.Manager):
+    pass
+
+
+
+# Ideally, we can store templates for the emails here, but for now, I'm going to leave it blank.
+class Email(models.Model):
+    objects = EmailManager()
