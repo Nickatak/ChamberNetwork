5b0a71874a1f01aedeaf576a68f2355bc9f457a1
Nickatak <nickle87@gmail.com>
2019-01-28 09:44:53 -0800

    settings.py: Install email app

--- chambernet/settings.py
+++ chambernet/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'apps.users',
     'apps.instruments',
     'apps.performances',
+    'apps.emails',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
