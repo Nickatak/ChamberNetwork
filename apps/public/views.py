9d0d0cf999776ac7b378fd3e943cf55807b505a4
Nickatak <nickle87@gmail.com>
2019-03-05 07:17:24 -0800

    views.py: Rename function

--- apps/public/views.py
+++ apps/public/views.py
@@ -96,7 +96,7 @@ def login_display(req):
 	return render(req, 'html/login.html', context)
 
 
-def pw_reset(req):
+def pw_reset_display(req):
     context = {
         'page_name' : 'Reset Password',
     }
